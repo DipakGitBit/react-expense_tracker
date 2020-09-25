6eaa727f2004d1d312fca52de63339eae977163a
dipakvit <dipakpaul199@gmail.com>
2020-09-25 22:40:24 +0530

    Updates

---
+++ precache-manifest.30001d1bf9290613e88f597919bb78ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58ba0bb17a070f4179dea7dc40017776",
+    "url": "/react-expense_tracker/index.html"
+  },
+  {
+    "revision": "fbb21bd0b47b73749dcf",
+    "url": "/react-expense_tracker/static/css/main.8751d968.chunk.css"
+  },
+  {
+    "revision": "ed17a32adde8c57834a6",
+    "url": "/react-expense_tracker/static/js/2.dc8e8cdb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense_tracker/static/js/2.dc8e8cdb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fbb21bd0b47b73749dcf",
+    "url": "/react-expense_tracker/static/js/main.ff76fb74.chunk.js"
+  },
+  {
+    "revision": "539a10d279bafaa41323",
+    "url": "/react-expense_tracker/static/js/runtime-main.15782614.js"
+  }
+]);
